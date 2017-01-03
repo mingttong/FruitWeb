824c3a48885058940d594b1d271923dc1aac12ff
mingttong <zhouwunan@qq.com>
2017-01-03 20:31:48 +0800

    完成增加、减少数目的交互

--- WebContent/js/cart.js
+++ WebContent/js/cart.js
@@ -2,32 +2,6 @@
  * Created by lenovo on 2017/1/2.
  */
 
-function submitPost(url, dataToPost) {
-
-    // 生成虚拟表单
-    var tmp_form = document.createElement("form");
-    tmp_form.action = url;
-    tmp_form.method = "post";
-    tmp_form.style.display = "none";
-
-    for (var dataName in dataToPost) {
-
-        // 避免搜索到原型链上的属性和方法
-        if (dataToPost.hasOwnProperty(dataName)) {
-            var option = document.createElement("textarea");
-            option.name = dataName;
-            option.value = dataToPost[dataName];
-
-            tmp_form.appendChild(option);
-        }
-    }
-
-    document.body.appendChild(tmp_form);
-    tmp_form.submit();
-
-    return tmp_form;
-}
-
 /**
  * 删除商品
  * @param anchor “删除商品按钮”的元素
@@ -49,6 +23,30 @@ function increaseItem(anchor) {
     var goods_id = anchor.getAttribute("data-sku");
     var url = "increaseItem.do?goodsID=" + goods_id;
 
+    // 获取更新前的商品对象
+    var itemObj = getItemObj(goods_id);
+
+    if (itemObj.getNum() === 1) {
+        // 增加前数量为1，则改变其样式
+
+        // 获取减少数量按钮并改变其样式
+        var decreaseBtn = itemObj.getItemElement().querySelector(".decrement");
+
+        if (decreaseBtn.classList) {
+            decreaseBtn.classList.remove("disabled");
+        } else {
+
+            // 通过改变字符串改变类名
+            // 懒得处理可能出现的多余空格
+            var newClass = decreaseBtn.getAttribute("class").replace("disabled", "");
+            decreaseBtn.setAttribute("class", newClass);
+
+        }
+
+        // 加回decreaseItem()事件
+        decreaseBtn.setAttribute("onclick", "decreaseItem(this)");
+    }
+
     ajaxUpdateItem(goods_id, url);
 }
 
@@ -61,21 +59,35 @@ function decreaseItem(anchor) {
     var goods_id = anchor.getAttribute("data-sku");
     var url = "decreaseItem.do?goodsID=" + goods_id;
 
-    var itemObj = getItemObj(goods_id);
+    // 获取同步通讯后的结果
+    var itemObj = ajaxUpdateItem(goods_id, url);
 
-    if (typeof itemObj.getNum() === "number" && itemObj.getNum() > 1) {
+    if (itemObj.getNum() === 1) {
+        // 减少后数量为1，则改变其样式
 
-        ajaxUpdateItem(goods_id, url);
+        // 获取按钮元素
+        var decreaseBtn = itemObj.getItemElement().querySelector(".decrement");
 
-    } else {
-        // 调整样式
+        // 添加样式
+        if (decreaseBtn.classList) {
+            decreaseBtn.classList.add("disabled");
+        } else {
+            // 通过改变字符串改变类名
+            // 懒得处理可能出现的多余空格
+            var newClass = decreaseBtn.getAttribute("class").concat(" disabled");
+            decreaseBtn.setAttribute("class", newClass);
+        }
+
+        // 取消掉decreaseItem()事件
+        decreaseBtn.removeAttribute("onclick");
     }
+
 }
 
 /**
- * 异步更新商品信息
- * @param id
- * @param url
+ * 同步更新商品信息
+ * @param id 商品id
+ * @param url 通讯的url
  */
 function ajaxUpdateItem(id, url) {
 
@@ -85,6 +97,7 @@ function ajaxUpdateItem(id, url) {
     var httpRequest = new XMLHttpRequest();
 
     httpRequest.onreadystatechange = (function () {
+
         if (httpRequest.readyState === 4 && httpRequest.status === 200) {
             // 加载成功后
 
@@ -106,17 +119,24 @@ function ajaxUpdateItem(id, url) {
                 itemObj.setSum(sum);
 
             }
+
         }
+
+
     });
 
-    httpRequest.open("GET", url, true);
+    // 被逼无奈用同步的方式，不然没其他办法。
+    // 再次获取节点元素也得等通讯完了才能获取，不然获取的还是老的值。
+    httpRequest.open("GET", url, false);
     httpRequest.send();
+
+    return itemObj;
 }
 
 /**
  * 获取商品对象
  * @param id
- * @returns {{getNum: Function, getSum: Function, setNum: Function, setSum: Function}}
+ * @returns {{num: Number, sum: Number, getNum: Function, getSum: Function, setNum: Function, setSum: Function, getItemElement: Function}}
  */
 function getItemObj(id) {
 
@@ -130,20 +150,36 @@ function getItemObj(id) {
     }
 
     return {
+        "num" : num,
+        "sum" : sum,
         "getNum" : function () {
-            return num;
+            return this.num;
         },
         "getSum" : function () {
-            return sum;
+            return this.sum;
         },
         "setNum" : function (num) {
             numElement.setAttribute("value", num);
+            this.num = num;
         },
         "setSum" : function (sum) {
             sumElement.textContent = sum;
+            this.sum = sum;
         },
         "getItemElement" : function () {
             return item;
         }
     };
 }
+
+window.onload = function () {
+
+    var inputElements = document.querySelectorAll(".p-quantity input");
+
+    for (var i = 0; i < inputElements.length; i++) {
+        inputElements[i].addEventListener("change", function () {
+            console.log("success");
+        });
+    }
+
+};
